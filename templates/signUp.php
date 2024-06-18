90ded43643326ad3b4d4bc6da415f7e39808fd19
Oriane Longeron <oriane.longeron@centrale.centralelille.fr>
2024-06-18 09:10:23 +0200

    Changement image login

---
+++ templates/signUp.php
@@ -0,0 +1,52 @@
+<?php
+
+// Si la page est appelée directement par son adresse, on redirige en passant pas la page index
+if (basename($_SERVER["PHP_SELF"]) != "index.php")
+{
+	header("Location:../index.php");
+	die("");
+}
+
+// On envoie l'entête Content-type correcte avec le bon charset
+header('Content-Type: text/html;charset=utf-8');
+
+// Pose qq soucis avec certains serveurs...
+echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+    <!-- **** H E A D **** -->
+    <head>	
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-15" />
+        <title>Covoit'Campus</title>
+        <link rel="stylesheet" type="text/css" href="CSS/style.css">
+    </head>
+    <!-- **** F I N **** H E A D **** -->
+
+
+    <!-- **** B O D Y **** -->
+    <body id="sguBody">
+
+        <div id="sguCont">
+            <h1 id=sguTittle>Covoit'Campus</h1>
+            <hr />
+
+            <form id="sguForm" action="controleur.php" methode="post">
+                <input class="sguInput" type="text" name="pseudo" placeholder="Pseudo..." />
+                <input class="sguInput" type="password" name="passe" placeholder="Mot de passe..." />
+                <input id="sguSubmit" type="submit" name="login" value="Se connecter" />
+            </form>
+
+            <div>Vous n'avez pas encore de compte ?</div>
+            <div id="sguSignUp">
+                <img src="ressources/examen.png" alt="Icon inscription" />
+                <a href="templates/signUp.php">INSCRIVEZ VOUS !</a>
+            </div>
+
+            <img id="sguLogo" src="ressources/ec-lille.png" alt="Logo Centrale" />
+        </div>
+        
+    </body>
+</html>
