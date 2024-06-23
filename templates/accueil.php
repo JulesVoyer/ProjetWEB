5eedb68209af734ec1a0f155d3b2fbfcac96f730
mandar69 <clementmanet@gmail.com>
2024-06-23 10:18:42 +0200

    Correctif Vue Accueil 2

--- templates/accueil.php
+++ templates/accueil.php
@@ -46,6 +46,7 @@ echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
         <p class="alignLeft">Tu peux consulter tes trajets passés et à venir dans l’onglet “historique”.</p>
         <h3>Profil</h3>
         <p class="alignRight">Tu peux modifier tes informations personnelles ou consulter tes véhicules dans l’onglet “profil”.</p>
+        <br><br><br>
     </div>
     
 
