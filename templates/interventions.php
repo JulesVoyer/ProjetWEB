29c5c6f552d30f4052ae0ca4a90c599ca2ba3019
mandar69 <clementmanet@gmail.com>
2024-06-24 19:38:15 +0200

    rename trajetdetail + create intervention

---
+++ templates/interventions.php
@@ -0,0 +1,28 @@
+<?php
+// Ce fichier permet de tester les fonctions développées dans le fichier bdd.php (première partie)
+
+// Si la page est appelée directement par son adresse, on redirige en passant pas la page index
+if (basename($_SERVER["PHP_SELF"]) == "interventions.php")
+{
+	header("Location:../index.php?view=header");
+	die("");
+}
+
+include_once("libs/modele.php");
+include_once("libs/maLibUtils.php");
+include_once("libs/maLibForms.php"); 
+
+// Hypo : l'user doit etre connecté 
+if (! valider("connecte","SESSION")) {
+	header("Location:?view=accueil&msg_feedback=" . urlencode("Il faut etre connecte !"));
+	die("");
+}
+
+?>
+
+    <!-- **** B O D Y **** -->
+<div id="interventionsBody">
+    <br><br><br>
+    edzezefzefzfzef
+    
+</div>
