ecb9a1393cc4e1e0e8e6b9eeb980dca798b98c10
julesvoyer <jules.voyer@everysens.com>
2024-06-24 20:17:02 +0200

    added CreateAccount action

--- controleur.php
+++ controleur.php
@@ -13,6 +13,76 @@ session_start();
 
 		switch($action)
 		{
+			//login de l'utilisateur
+			case "Login":
+				//on récupère les valeurs du formulaire
+				$login = valider("login");
+				$mdp = valider("mdp");
+				//on vérifie si le login et le mot de passe sont corrects
+				$profil = verifUser($login, $mdp);
+				//si le profil existe
+				if ($profil)
+				{
+					//on redirige vers la page d'accueil
+					$qs = "?view=accueil";
+				}
+				else
+				{
+					//sinon on redirige vers la page de login avec un message d'erreur
+					$qs = "?view=login&erreur=".urlencode("Login ou mot de passe incorrect");
+				}
+			break;
+			//fin login
+			
+			//logout de l'utilisateur
+			case "Logout":
+
+				session_destroy();
+				$qs = "?view=login";
+			break;
+			//fin logout
+
+			//création d'un compte
+			case "CreateAccount" :
+				if (valider("username") && valider("password") && valider("password_confirmation") && valider("display_name") && valider("driving_license") && valider("street_number") && valider("street") && valider("city") && valider("city_code"))
+				{
+					// On récupère les valeurs du formulaire
+					$username = valider("username");
+					$password = valider("password");
+					$password_confirm = valider("password_confirmation");
+					$display_name = valider("display_name");
+					$driving_license = valider("driving_license");
+					$street_number = valider("street_number");
+					$street = valider("street");
+					$city = valider("city");
+					$city_code = valider("city_code");
+
+					//on vérifie que le nom d'utilisateur n'est pas déjà pris
+					if (checkUsername($username)){
+						//si le nom d'utilisateur est déjà pris on redirige vers la page de création de compte avec un message d'erreur
+						$qs = "?view=createAccount&erreur=".urlencode("Nom d'utilisateur déjà pris");
+					}
+					else{
+
+						//on vérifie que l'utilisateur est bien sûr de son mot de passe 
+						if($password == $password_confirmation){
+							createAccount($username, $password, $display_name, $driving_license, $street_number,$street, $city, $city_code);
+							$qs = "?view=login";
+						}
+						else
+						{
+							//sinon on redirige vers la page de création de compte avec un message d'erreur
+							$qs = "?view=createAccount&erreur=".urlencode("Les mots de passe ne correspondent pas");
+						}
+					}
+				}
+				else
+				{
+					//sinon on redirige vers la page de création de compte avec un message d'erreur
+					$qs = "?view=createAccount&erreur=".urlencode("Veuillez remplir tous les champs");
+				}
+			break;
+
 
 
 		} // fin switch(action)
