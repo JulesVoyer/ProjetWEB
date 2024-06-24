f271b62f086dfb7a56231e0f4b41a97d465526be
mandar69 <clementmanet@gmail.com>
2024-06-24 19:04:23 +0200

    Modif début du code des vues

--- templates/conversations.php
+++ templates/conversations.php
@@ -1,37 +1,27 @@
 <?php
+// Ce fichier permet de tester les fonctions développées dans le fichier bdd.php (première partie)
 
 // Si la page est appelée directement par son adresse, on redirige en passant pas la page index
-if (basename($_SERVER["PHP_SELF"]) != "index.php")
+if (basename($_SERVER["PHP_SELF"]) == "conversations.php")
 {
-	header("Location:../index.php");
+	header("Location:../index.php?view=users");
 	die("");
 }
 
 include_once("libs/modele.php");
 include_once("libs/maLibUtils.php");
-include_once("libs/maLibForms.php");
+include_once("libs/maLibForms.php"); 
 
-// On envoie l'entête Content-type correcte avec le bon charset
-header('Content-Type: text/html;charset=utf-8');
+// Hypo : l'user doit etre connecté 
+if (! valider("connecte","SESSION")) {
+	header("Location:?view=accueil&msg_feedback=" . urlencode("Il faut etre connecte !"));
+	die("");
+}
 
-// Pose qq soucis avec certains serveurs...
-echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-    <!-- **** H E A D **** -->
-    <head>	
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-15" />
-        <title>Covoit'Campus</title>
-        <link rel="stylesheet" type="text/css" href="CSS/style.css">
-    </head>
-    <!-- **** F I N **** H E A D **** -->
-
-
     <!-- **** B O D Y **** -->
-    <body id="convBody">
+<div id="convBody">
         <br><br><br><br>
 
         <div id="titlePage">Mes conversations</div>
@@ -68,5 +58,4 @@ echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
             </div>
         </a>
     
-    </body>
-</html>
+</div>
--- templates/profil.php
+++ templates/profil.php
@@ -1,40 +1,29 @@
 <?php
+// Ce fichier permet de tester les fonctions développées dans le fichier bdd.php (première partie)
 
 // Si la page est appelée directement par son adresse, on redirige en passant pas la page index
-if (basename($_SERVER["PHP_SELF"]) != "index.php")
+if (basename($_SERVER["PHP_SELF"]) == "profil.php")
 {
-	header("Location:../index.php");
+	header("Location:../index.php?view=users");
 	die("");
 }
 
 include_once("libs/modele.php");
-include_once("libs/maLibUtils.php");
-include_once("libs/maLibForms.php");
+include_once("libs/maLibUtils.php"); // tprint
+include_once("libs/maLibForms.php"); 
+// définit mkTable
 
-// On envoie l'entête Content-type correcte avec le bon charset
-header('Content-Type: text/html;charset=utf-8');
-
-// Pose qq soucis avec certains serveurs...
-echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
+// Hypo : l'user doit etre connecté 
+if (! valider("connecte","SESSION")) {
+	header("Location:?view=accueil&msg_feedback=" . urlencode("Il faut etre connecte !"));
+	die("");
+}
+ 
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-    <!-- **** H E A D **** -->
-    <head>	
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-15" />
-        <title>Covoit'Campus</title>
-        <link rel="stylesheet" type="text/css" href="CSS/style.css">
-    </head>
-    <!-- **** F I N **** H E A D **** -->
-
-
     <!-- **** B O D Y **** -->
-    <body id="profilBody">
+    <div id="profilBody">
         <br><br><br><br>
 
 
-    
-    </body>
-</html>
+    </div>
