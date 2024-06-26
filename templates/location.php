cca9016aa78b8dcec03a337cf84014f5d4ae3636
mandar69 <clementmanet@gmail.com>
2024-06-26 15:14:19 +0200

    Vue location debut

--- templates/location.php
+++ templates/location.php
@@ -2,9 +2,9 @@
 // Ce fichier permet de tester les fonctions développées dans le fichier bdd.php (première partie)
 
 // Si la page est appelée directement par son adresse, on redirige en passant pas la page index
-if (basename($_SERVER["PHP_SELF"]) == "location.php")
+if (basename($_SERVER["PHP_SELF"]) == "historique.php")
 {
-	header("Location:../index.php?view=location");
+	header("Location:../index.php?view=historique");
 	die("");
 }
 
@@ -12,18 +12,11 @@ include_once("libs/modele.php");
 include_once("libs/maLibUtils.php");
 include_once("libs/maLibForms.php"); 
 
-// Hypo : l'user doit etre connecté 
-if (! valider("connecte","SESSION")) {
-	header("Location:?view=accueil&msg_feedback=" . urlencode("Il faut etre connecte !"));
-	die("");
-}
-
 ?>
 
     <!-- **** B O D Y **** -->
 <div id="locationBody">
     <br><br><br>
-    azazazdadaz
 
 	<a href="index.php?view=trajetsDetails" class="trajet">
         <img id="autoRouge" src="ressources/auto-rouge.png" alt="icone voiture rouge" style="display: none;"/>
@@ -102,6 +95,5 @@ if (! valider("connecte","SESSION")) {
                 </div>
                     
             </a>
-zdazdazdazdazd
     
 </div>
